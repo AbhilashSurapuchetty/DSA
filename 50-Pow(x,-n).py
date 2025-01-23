62adb753e78d126f34fbf256dce04d0ef2bab56e
Abhilash Surapuchetty <alash0849@gmail.com>
2025-01-23 12:50:48 +0530

    [50] [Time Beats: 1.96%] [Memory Beats: 16.02%] - LeetPush

---
+++ 50-Pow(x,-n).py
@@ -0,0 +1,11 @@
+import math
+class Solution(object):
+    def myPow(self, x, n):
+        \\\
+        :type x: float
+        :type n: int
+        :rtype: float
+        \\\
+        x = pow(x,n)
+        return x
+        
