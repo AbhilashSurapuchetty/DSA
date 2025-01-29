8eb1239119e1c2cd9611244f4b7e31f1b39b1c78
Abhilash Surapuchetty <alash0849@gmail.com>
2025-01-29 15:32:06 +0530

    [39] [Time Beats: 84.25%] [Memory Beats: 29.41%] - LeetPush

--- 39-Combination-Sum.py
+++ 39-Combination-Sum.py
@@ -2,20 +2,22 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         l = []
         l1 = []
-        self.helper(0,candidates,target,l,l1)
+        self.helper(candidates,0,l,l1,target)
         return l
+         
 
-    def helper(self,i,arr,target,l,l1) :
+    def helper(self,arr,i,l,l1,target) :
         if (i == len(arr)) :
+
             if (target == 0) :
                 l.append(l1.copy())
             return
-        
+
         if (arr[i] <= target) :
             l1.append(arr[i])
-            self.helper(i,arr,target - arr[i],l,l1)
-            l1.remove(arr[i])
-        self.helper(i+1,arr,target,l,l1)
+            self.helper(arr,i,l,l1,target-arr[i])
+            l1.pop()
+        self.helper(arr,i+1,l,l1,target)
         
 
 
